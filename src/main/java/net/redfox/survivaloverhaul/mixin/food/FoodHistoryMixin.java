f9a51e3e8c2a1b4251e9edbb4a709193c3fcdcc1
Peter <85247098+PeterLangendorf@users.noreply.github.com>
2026-02-20 13:52:39 -0500

    Add check for food history enabled

--- src/main/java/net/redfox/survivaloverhaul/mixin/food/FoodHistoryMixin.java
+++ src/main/java/net/redfox/survivaloverhaul/mixin/food/FoodHistoryMixin.java
@@ -7,6 +7,7 @@ import net.minecraft.world.food.FoodProperties;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.redfox.survivaloverhaul.client.ClientFoodHistoryData;
+import net.redfox.survivaloverhaul.config.ModCommonConfigs;
 import net.redfox.survivaloverhaul.food.foodHistory.PlayerFoodHistory;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -16,6 +17,7 @@ import org.spongepowered.asm.mixin.injection.Redirect;
 public abstract class FoodHistoryMixin {
   @Redirect(method = "eat(Lnet/minecraft/world/level/Level;Lnet/minecraft/world/item/ItemStack;)Lnet/minecraft/world/item/ItemStack;", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/food/FoodData;eat(Lnet/minecraft/world/item/Item;Lnet/minecraft/world/item/ItemStack;Lnet/minecraft/world/entity/LivingEntity;)V"))
   private void modifyFoodStats(FoodData instance, Item item, ItemStack stack, LivingEntity entity) {
+    if (!ModCommonConfigs.FOOD_HISTORY_ENABLED.get()) return;
     final double multiplier = PlayerFoodHistory.getMultiplierAndSum(ClientFoodHistoryData.get(), PlayerFoodHistory.getItemNameFromStack(stack)).getB();
     if (item.isEdible()) {
       FoodProperties properties = stack.getFoodProperties(entity);
